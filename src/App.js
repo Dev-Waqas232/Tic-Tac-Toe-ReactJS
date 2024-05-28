5df2c9b21a3ea937568b8f3200201b2546d11178
waqas <wmunir232@gmail.com>
2024-05-28 21:28:22 +0500

    Added Initial Context

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import Board from "./components/Board";
 import GameInfo from "./components/GameInfo";
+import { GameProvider } from "./context/GameContext";
 
 const App = () => {
   return (
-    <main className="grid md:grid-cols-2 bg-bgPrimary w-full h-screen px-20">
-      <section className="flex flex-col h-full justify-center items-center">
-        <Board />
-      </section>
-      <section className="flex flex-col h-full justify-center items-center">
-        <GameInfo />
-      </section>
-    </main>
+    <GameProvider>
+      <main className="grid md:grid-cols-2 bg-bgPrimary w-full h-screen px-20">
+        <section className="flex flex-col h-full justify-center items-center">
+          <Board />
+        </section>
+        <section className="flex flex-col h-full justify-center items-center">
+          <GameInfo />
+        </section>
+      </main>
+    </GameProvider>
   );
 };
 
--- src/context/GameContext.jsx
+++ src/context/GameContext.jsx
@@ -3,3 +3,7 @@ import { createContext, useContext } from "react";
 const GameContext = createContext();
 
 export const useGame = () => useContext(GameContext);
+
+export const GameProvider = ({ children }) => {
+  return <GameContext.Provider>{children}</GameContext.Provider>;
+};
